e6cc5421ff864e8d13ed716f75e334b6e003d9ca
Roman Rafikov <romanrara@yandex.ru>
2023-03-10 13:24:08 +0500

    Fixed bugs

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,15 @@ let profileProf = document.querySelector('.profile__profession');
 let saveButton = document.querySelector('.popup__save-button');
 
 editButton.addEventListener('click', function() {
-    popup.classList.add('popup__opened')
+    popup.classList.add('popup_opened')
 });
 closeButton.addEventListener('click', function() {
-    popup.classList.remove('popup__opened')
+    popup.classList.remove('popup_opened')
 });
 
 let formElement = document.querySelector('.popup__container');
-let NamePopup = formElement.querySelector('.popup__name');
-let JobPopup = formElement.querySelector('.popup__profession');
+let NamePopup = formElement.querySelector('.popup__text');
+let JobPopup = formElement.querySelector('.popup__text-last');
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
