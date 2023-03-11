309959d9b4637c79e36396cf17fa591c8a74521f
Roman Rafikov <romanrara@yandex.ru>
2023-03-12 01:21:45 +0500

    Fixed bugs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,22 +4,31 @@ let closeButton = document.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileProf = document.querySelector('.profile__profession');
 let saveButton = document.querySelector('.popup__save-button');
-
-editButton.addEventListener('click', function() {
-    popup.classList.add('popup_opened')
-});
-closeButton.addEventListener('click', function() {
-    popup.classList.remove('popup_opened')
-});
-
 let formElement = document.querySelector('.popup__container');
-let NamePopup = formElement.querySelector('.popup__text');
-let JobPopup = formElement.querySelector('.popup__text-last');
+let NamePopup = formElement.querySelector('.popup__input_type_name');
+let JobPopup = formElement.querySelector('.popup__input_type_job');
 
-function handleFormSubmit (evt) {
+
+function OpenPopup() {
+    console.log(profileName.value)
+    NamePopup.value = profileName.textContent;
+    JobPopup.value = profileProf.textContent;
+    popup.classList.add('popup_opened')
+}
+
+function ClosePopup() {
+    popup.classList.remove('popup_opened')
+}
+
+editButton.addEventListener('click', OpenPopup);
+closeButton.addEventListener('click', ClosePopup);
+
+function EditPopup(evt) {
     evt.preventDefault(); 
     profileName.textContent = NamePopup.value;
     profileProf.textContent = JobPopup.value;
+    ClosePopup();
 }
 
-saveButton.addEventListener('click', handleFormSubmit);
+
+formElement.addEventListener('submit', EditPopup);
